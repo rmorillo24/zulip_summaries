471092f9b8718d3e09c480f4876ac353f4346401
rmorillo24 <rmorillo@gmail.com>
2024-02-17 14:34:52 +0100

    trying retreiver

--- rag.py
+++ rag.py
@@ -17,6 +17,7 @@ import warnings
 import rag
 import json
 import logger as log
+import os
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
@@ -35,8 +36,19 @@ SUMMARY_TEMPLATE = (
     # "Answer: "
 )
 
+ZULIP_QUERY_TEMPLATE=(
+    "context information is below.\n"
+    "---------------------\n"
+    "{context_str}\n"
+    "---------------------\n"
+    "There are different topics discussed in the information provided.\n"
+    "Use only the knowledge included in the topics to answer the user's question\n"
+    "user's question: {query_str}\n"
+    "Answer: "
+)
+
 class Rag:
-    def __init__(self, config_path='./.venv/config.yml', model_name = "llama2"):
+    def __init__(self, config_path='./.venv/config.yml'):
         
         with open(config_path, 'r', encoding='utf8') as ymlfile:
             self.cfg = box.Box(yaml.safe_load(ymlfile))
@@ -62,48 +74,38 @@ class Rag:
             vector_store=self.vector_store
         )
         summary_prompt_template = PromptTemplate(SUMMARY_TEMPLATE)
-        self.response_synthesizer = get_response_synthesizer(
+        self.summary_response_synthesizer = get_response_synthesizer(
             response_mode="tree_summarize",
             use_async=False,
             service_context=self.service_context,
             summary_template=summary_prompt_template
-        )        
+        )
+        self.zulip_question_prompt_template = PromptTemplate(ZULIP_QUERY_TEMPLATE)
+        self.zulip_question_response_synthesizer = get_response_synthesizer(
+            # response_mode="tree_summarize",
+            use_async=False,
+            service_context=self.service_context,
+            text_qa_template=self.zulip_question_prompt_template,
+        )
+
         self.index_main = VectorStoreIndex.from_vector_store(
             vector_store = self.vector_store,
             service_context = self.service_context
         )
         self.retriever = VectorIndexRetriever(
             index=self.index_main,
-            similarity_top_k=10,
+            similarity_top_k=3,
         )        
         self.zulip_query_engine = self.index_main.as_query_engine()
         # self.zulip_query_engine = RetrieverQueryEngine(
         #     retriever=self.retriever,
-        #     response_synthesizer=self.response_synthesizer,
+        #     response_synthesizer=self.zulip_question_response_synthesizer,
         #     node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)]
         # )        
         self.index_doc = None
         self.documents = []
         self.doc_query_engine = None
 
-
-    def set_model(self, model_name):
-        self.cfg.LLM = model_name
-        self.llm = Ollama(
-            model=self.cfg.LLM,
-            base_url=self.cfg.OLLAMA_BASE_URL,
-            temperature=self.cfg.TEMPERATURE)
-        self.service_context = ServiceContext.from_defaults(
-            embed_model=self.embeddings,
-            llm=self.llm)
-        self.response_synthesizer = get_response_synthesizer(
-            response_mode="tree_summarize",
-            use_async=False,
-            service_context=self.service_context)
-        
-        self.index_main = VectorStoreIndex.from_vector_store(
-            vector_store = self.vector_store,
-            service_context = self.service_context)
         
 
     def get_summary(self, title): 
@@ -111,7 +113,7 @@ class Rag:
         doc_summary_index = DocumentSummaryIndex.from_documents(
             self.documents,
             service_context=self.service_context,
-            response_synthesizer=self.response_synthesizer,
+            response_synthesizer=self.summary_response_synthesizer,
             show_progress=True)
         print("Building summary index...")
         doc_summary = doc_summary_index.get_document_summary(doc_id = title)
@@ -130,7 +132,7 @@ class Rag:
         self.index_doc = VectorStoreIndex.from_documents(self.documents,
                                                          service_context=self.service_context,
                                                          llm = self.llm,
-                                                         show_progress=True)
+                                                         show_progress=True,)
         self.doc_query_engine = self.index_doc.as_query_engine()
 
 
@@ -142,7 +144,22 @@ class Rag:
     def ask_zulip(self, question):
         print("Asking zulip...")
         return self.zulip_query_engine.query(question)
-    
+
+
+
+
+    def ingest_docs_in_folder(self, folder_name, max_files = 100000):
+        processed_files = 0
+        for filename in os.listdir(folder_name):
+            if (processed_files <= max_files):
+                file_path = os.path.join(folder_name, filename)
+                logger.info("__________________________Processing file: " + file_path)
+                if os.path.isfile(file_path):
+                    text = ""
+                    with open(file_path, 'r') as file:
+                        text = " ".join(line.rstrip() for line in file)
+                    self.ingest_text(text, filename)
+                    processed_files += 1
 
 if __name__ == "__main__":
     logger = log.init_logger(__name__)
@@ -150,13 +167,8 @@ if __name__ == "__main__":
 
     rag = Rag()
 
-    # title = "MYTESTTITLE"
-    # with open('test.txt', 'r') as file:
-    #     text = " ".join(line.rstrip() for line in file)
-
-    # summary = rag.ingest_text(text, title)
-
-    # query
+    # rag.ingest_docs_in_folder("./data/balena-io___os")
+    
     while True:
         try:
             question = input("\n\n> ")
